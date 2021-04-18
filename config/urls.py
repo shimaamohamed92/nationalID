1ab708bf9a6d1f2f7166858e30c6c5eae5294893
shimaamohamed92 <shimaa.mo7amed92@gmail.com>
2021-04-19 01:39:32 +0200

    add static to urls

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
